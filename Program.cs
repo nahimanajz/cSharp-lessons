55bc1114865d1a0f49a62fce106da91f92101a4e
janvierbwk <na-janvier@bw-africa.com>
2022-11-02 08:50:32 +0200

    Dealing with Maths

--- Program.cs
+++ Program.cs
@@ -34,6 +34,22 @@ using System;
             Console.WriteLine($"To upper{greet.ToUpper()}");
             Console.WriteLine($"To Lower{greet.ToLower()}");
 
+            // Searching strings 
+            string songLyrics = "You said goodbye and I said Hello";
+            var hasString = songLyrics.Contains("goodbye");
+            var endsWithString = songLyrics.EndsWith("goodbye");
+            var startsWithString = songLyrics.StartsWith("You");
+
+            Console.WriteLine($"{hasString} starts with: {startsWithString} ends with: {endsWithString}");
+            Console.WriteLine($"starts with goodbye: {startsWithString}");
+            Console.WriteLine($"ends with You: {endsWithString}");
+
+            // Numbers, Integers and Math
+            int a = 3, b=4, c= 5;
+            int multiplyFirst = a+b*c;
+            int sumFirst = (a+b) * c;
+            Console.WriteLine($"{multiplyFirst}");
+            Console.WriteLine($"{sumFirst}");
         }
     }
 
