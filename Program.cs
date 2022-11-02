55999a8002e069fa2f64def8f91666ef71900f1d
janvierbwk <na-janvier@bw-africa.com>
2022-11-02 08:01:58 +0200

    intro: Hello world application

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+    class Test{
+        static void Main(string[] args){
+            string name = "Janvier";
+
+            Console.WriteLine(name);
+            Console.WriteLine("Hello " +name); // string concantanation
+            Console.WriteLine($"Hello {name}"); // string interpolation
+        }
+    }
+
