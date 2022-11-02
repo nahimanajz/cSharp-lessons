edd7d1f092a478241cfd4129291b25a70f1a4d43
janvierbwk <na-janvier@bw-africa.com>
2022-11-02 08:26:48 +0200

    Add string manipulations

--- Program.cs
+++ Program.cs
@@ -2,11 +2,38 @@
 using System;
     class Test{
         static void Main(string[] args){
+            // Section one
             string name = "Janvier";
 
             Console.WriteLine(name);
             Console.WriteLine("Hello " +name); // string concantanation
             Console.WriteLine($"Hello {name}"); // string interpolation
+             
+             // Section Two 
+             string greet = "  Hello world.  ";
+            //size
+            Console.WriteLine($"my variable size is: {greet.Length}");
+            // replace
+            greet = greet.Replace("Hello", "Muraho neza");
+            Console.WriteLine($"{greet}");
+
+            // trimming [start, end, all]
+            Console.WriteLine($"[###{greet}###]");
+            string trimmed = greet.TrimStart();
+            Console.WriteLine($"[##{trimmed}##]");
+
+            // trim end
+            string trimmedEnd = greet.TrimEnd();
+            Console.WriteLine($"[##{trimmedEnd}##]");
+
+            // trim both sides
+            string trimBothEnds = greet.Trim();
+            Console.WriteLine($"[##{trimBothEnds}##]");
+
+            // capitalizing
+            Console.WriteLine($"To upper{greet.ToUpper()}");
+            Console.WriteLine($"To Lower{greet.ToLower()}");
+
         }
     }
 
