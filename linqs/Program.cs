80dfe6aef5bedf9ae5e0ab52daae2da68617b66e
janvierbwk <na-janvier@bw-africa.com>
2023-01-06 12:44:08 +0200

    add mvc app

--- linqs/Program.cs
+++ linqs/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+IList<Student> studentList = new List<Student>() { 
+        new Student() { StudentID = 1, StudentName = "John", Age = 13} ,
+        new Student() { StudentID = 2, StudentName = "Moin",  Age = 21 } ,
+        new Student() { StudentID = 3, StudentName = "Bill",  Age = 18 } ,
+        new Student() { StudentID = 4, StudentName = "Ram" , Age = 20} ,
+        new Student() { StudentID = 5, StudentName = "Ron" , Age = 15 } 
+    };
+
+var filteredResult = from s in studentList
+                    where s.Age > 12 && s.Age < 20
+                    
+                    select s;
+foreach ( var  st in filteredResult) Console.WriteLine(st.Age);           
+
+// Filter by using delegate
+Func<Student, bool> isUnderAge= delegate(Student st) {
+    return st.Age < 18;
+};
+
+var filter  = from s in studentList
+                        where isUnderAge(s) 
+                        select s;
+foreach ( var  stu in filter) Console.WriteLine($" Under age student: {stu.StudentName}");           
---
+++ linqs/Readme.md
@@ -0,0 +1,1 @@
+https://www.tutorialkart.com/c-sharp-tutorial/create-csharp-project-with-visual-studio-code/
